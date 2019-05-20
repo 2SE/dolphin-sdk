4b50b078121e0dd2b98ab42fbb93ba1854718e26
rennbon <rennbon.zhu@gmail.com>
2019-05-20 20:54:43 +0800

    feat(mq): kafka dirver

--- mq/kafka/kafka.go
+++ mq/kafka/kafka.go
@@ -1,6 +1,7 @@
 package kafka
 
 import (
+	"context"
 	"fmt"
 	"github.com/segmentio/kafka-go"
 	"time"
@@ -16,8 +17,24 @@ type Config struct {
 	MaxBytes  int
 	MaxWait   time.Duration
 }
+type WriteConfig struct {
+	Brokers []string
+	Topic   string
+	//retry times
+	MaxAttempts int
+	//队列容量
+	QueueCapacity int
+}
 
-func NewKafka(cnf *Config) *kafka.Reader {
+func NewKafkaReader(cnf *Config) *kafka.Reader {
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+	for k, v := range cnf.Brokers {
+		_, err := kafka.DialContext(ctx, "tcp", v)
+		if err != nil {
+			panic(fmt.Errorf("the broker dial err %v and the index is %d", err, k))
+		}
+	}
+
 	reader := kafka.NewReader(kafka.ReaderConfig{
 		Brokers:   cnf.Brokers,
 		GroupID:   cnf.GroupID,
@@ -32,4 +49,16 @@ func NewKafka(cnf *Config) *kafka.Reader {
 		panic(fmt.Errorf("kafka topic %s consumer err:%s", cnf.Topic, err.Error()))
 	}
 	return reader
+}
+
+func NewKafkaWriter(c *kafka.WriterConfig) *kafka.Writer {
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+	for k, v := range c.Brokers {
+		_, err := kafka.DialContext(ctx, "tcp", v)
+		if err != nil {
+			panic(fmt.Errorf("the broker dial err %v and the index is %d", err, k))
+		}
+	}
+	writer := kafka.NewWriter(*c)
+	return writer
 }
--- mq/kafka/kafka_test.go
+++ mq/kafka/kafka_test.go
@@ -2,13 +2,36 @@ package kafka
 
 import (
 	"context"
+	"github.com/segmentio/kafka-go"
 	"testing"
+	"time"
 )
 
-func TestNewKafka(t *testing.T) {
-	reader := NewKafka(&Config{
+func TestNewKafkaReader(t *testing.T) {
+	reader := NewKafkaReader(&Config{
 		Brokers: []string{"192.168.10.189:9092"},
-		Topic:   "topic01",
+		Topic:   "dolphinhub",
+		MaxWait: time.Second * 2,
 	})
+
 	reader.ReadMessage(context.Background())
+}
+
+//{0 0 0 0 0 0 {0s 0s 0s} {0s 0s 0s} {0s 0s 0s} {0 0 0} {0 0 0} 10 100 1s 10s 10s 15s 0 false 0 100  topic01}
+//{0 0 0 0 0 0 {0s 0s 0s} {0s 0s 0s} {0s 0s 0s} {0 0 0} {0 0 0} 10 100 1s 10s 10s 15s 0 false 0 100  dolphinhub}
+func TestNewKafkaWriter(t *testing.T) {
+	writer := NewKafkaWriter(&kafka.WriterConfig{
+		Brokers: []string{"192.168.10.189:9092"},
+		Topic:   "dolphinhub",
+	})
+
+	return
+	err := writer.WriteMessages(context.Background(), kafka.Message{
+		Key:   []byte("key"),
+		Value: []byte("val"),
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
 }
